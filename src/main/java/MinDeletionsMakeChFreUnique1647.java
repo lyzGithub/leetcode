d462c13891f1509541a043da5c8b733689e3e4ac
Yize Li <liyizeyouxiang@live.cn>
2020-11-17 16:33:51 +0800

    leet code 1647 1648

---
+++ src/main/java/MinDeletionsMakeChFreUnique1647.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MinDeletionsMakeChFreUnique1647
+{
+    public static void main(String[] args)
+    {
+        String s = "aaabbbcc";
+        System.out.println(minDeletions(s));
+    }
+
+    public static int minDeletions(String s)
+    {
+        char[] chs = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : chs) {
+            int count = 1;
+            if (map.containsKey(ch)) {
+                count += map.get(ch);
+            }
+            map.put(ch, count);
+        }
+        List<Integer> all = new ArrayList<>(map.values());
+        Collections.sort(all);
+        int count = 0;
+        for (int i = all.size() - 1; i >= 0; i--) {
+            int unique = all.get(i);
+            all.set(i, -1);
+            while (unique != 0 && all.contains(unique)) {
+                unique--;
+                count++;
+            }
+            all.set(i, unique);
+        }
+        return count;
+    }
+}
---
+++ src/main/java/SellDiminishingValuedColoredDalls1648.java
@@ -0,0 +1,54 @@
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class SellDiminishingValuedColoredDalls1648
+{
+    public static void main(String[] args)
+    {
+        int[] inventory = {1000000000};
+        int order = 1000000000;
+        System.out.println(maxProfit(inventory, order));
+    }
+
+    public static final int MOD = 1000000007;
+
+    public static int maxProfit(int[] inventory, int orders)
+    {
+        Arrays.sort(inventory);
+        int inventoryLength = inventory.length;
+        long result = 0;
+        if (inventory == null || inventory.length == 0) {
+            return 0;
+        }
+        if (inventory.length == 1) {
+            if (inventory[0] >= orders) {
+                return BigInteger.valueOf(inventory[0]).add(BigInteger.valueOf(inventory[0] - orders + 1))
+                        .multiply(BigInteger.valueOf(orders)).divide(BigInteger.valueOf(2)).mod(BigInteger.valueOf(MOD)).intValue();
+            }
+            else {
+                return BigInteger.valueOf(inventory[0]).multiply(BigInteger.valueOf(orders))
+                        .divide(BigInteger.valueOf(2)).mod(BigInteger.valueOf(MOD)).intValue();
+            }
+        }
+        for (int i = 0; i < orders; i++) {
+            int max = inventory[inventoryLength - 1];
+            if (max <= 0) {
+                return (int) result % MOD;
+            }
+            result = (result + max);
+            if (result >= MOD) {
+                result = result % MOD;
+            }
+            inventory[inventoryLength - 1] = max - 1;
+            int now = inventoryLength - 1;
+            while (now > 0 && inventory[now] < inventory[now - 1]) {
+                int temp = inventory[now - 1];
+                inventory[now - 1] = inventory[now];
+                inventory[now] = temp;
+                now--;
+            }
+        }
+        return (int) (result % MOD);
+    }
+}
